9d0545da62c1c757cf33fc39dd6057bbaff66cb5
Kat <katpeace@protonmail.com>
2024-06-23 19:49:55 -0400

    add more content

--- src/components/more/More.tsx
+++ src/components/more/More.tsx
@@ -1,12 +1,34 @@
+import Link from "next/link";
+import { CakeIcon, VideoIcon } from "../icons/heroIcons";
+import moreItems from "./moreItems";
+
 const More = () => {
   return (
-    <div className="main-container mt-12">
-      <h2 className="mb-4 self-center text-3xl">More</h2>
-      {/* <div>https://philarchive.org/archive/BECTEA-6</div>
-      <div>the million year yen button thing</div>
-      <div>https://www.lesswrong.com/leastwrong</div>
-      <div>trolley problem website</div>
-      <div>Kurzgesagt</div> */}
+    <div className="main-container mt-12 mb-16">
+      <h2 className="mb-4 self-center text-3xl">More Fun Thinkin' Spots</h2>
+      <p>
+        Looking for more philosophy stuff that isn't just WALLS OF TEXT? I got
+        ya!
+      </p>
+      <ul className="my-8 flex flex-col gap-8">
+        {moreItems.map((item) => {
+          return (
+            <li key={item.title}>
+              <Link
+                href={item.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center gap-2 text-pool-500"
+              >
+                {item.icon}
+                <h3 className="text-2xl">{item.title}</h3>
+              </Link>
+              {item.description}
+            </li>
+          );
+        })}
+      </ul>
+      <p>Have more suggestions? Let me know! ⬅ contact will go here someday</p>
     </div>
   );
 };
---
+++ src/components/more/moreItems.tsx
@@ -0,0 +1,78 @@
+import Link from "next/link";
+import { CakeIcon, ChatIcon, VideoIcon } from "../icons/heroIcons";
+
+const moreItems = [
+  {
+    title: "Absurd Trolley Problems",
+    href: "https://neal.fun/absurd-trolley-problems",
+    icon: <CakeIcon />,
+    description: (
+      <p>
+        A site where animated stick figures respond to your choices in wild
+        variations on the classic{" "}
+        <Link href="/experiments/trolley-problem">Trolley Problem</Link> .
+      </p>
+    ),
+  },
+  {
+    title: "The Million Yen Button",
+    href: "https://www.youtube.com/watch?v=RbCerMBxUFM",
+    icon: <VideoIcon />,
+    description: (
+      <p>
+        This video depicts a single thought experiment, but text cannot do it
+        justice. <span className="font-bold">Warning: may cause 😨</span>.
+      </p>
+    ),
+  },
+  {
+    title: "Your Logical Fallacy Is",
+    href: "https://yourlogicalfallacyis.com/",
+    icon: <CakeIcon />,
+    description: (
+      <p>
+        This site makes it fun and easy to learn about different logical
+        fallacies that can trip us up. Or to share them with people who won't
+        stop being wrong. Not that I did it.
+      </p>
+    ),
+  },
+  {
+    title: "Kurzgesagt",
+    href: "https://www.youtube.com/@kurzgesagt",
+    icon: <VideoIcon />,
+    description: (
+      <p>
+        Gorgeous, well-researched animated videos on topics in science,
+        philosophy, etc. Example titles include, "The Paradox of an Infinite
+        Universe," "Loneliness," and, "The Most Dangerous Weapon is Not
+        Nuclear."
+      </p>
+    ),
+  },
+  {
+    title: "Less Wrong",
+    href: "https://www.lesswrong.com/leastwrong",
+    icon: <ChatIcon />,
+    description: (
+      <p>
+        A forum site that aims to encourage rational thought. I haven't much
+        engaged with it, so I can't speak to any issues. But it surely full of
+        ideas.
+      </p>
+    ),
+  },
+  {
+    title: "Justice with Michael Sandel ",
+    href: "https://www.youtube.com/playlist?list=PL30C13C91CFFEFEA6",
+    icon: <VideoIcon />,
+    description: (
+      <p>
+        A classic Harvard lecture series. Prof. Sandel presents foundational
+        philosophers and ideas to the packed room, and students grab the mic to
+        weigh in with their thoughts.
+      </p>
+    ),
+  },
+];
+export default moreItems;
