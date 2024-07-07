1de8ce8621e86bcd92bbc7f4c174f25f92d18630
Kat <katpeace@protonmail.com>
2024-07-06 23:03:58 -0400

    email format

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -23,8 +23,11 @@ const About = () => {
         <div className="p-6 flex-1 flex flex-col gap-2 border border-dusky-400 rounded-lg">
           <h3 className="text-2xl text-center">✒️ Contact</h3>
           <p>
-            Ideas? Polite criticisms? Lucrative offers? Contact me at
-            <a href="mailto:thought_experiment_explorer@proton.me">
+            Ideas? Polite criticisms? Lucrative offers? Contact me at{" "}
+            <a
+              href="mailto:thought_experiment_explorer@proton.me"
+              className="text-pool-500 font-medium"
+            >
               thought_experiment_explorer@proton.me
             </a>
           </p>
