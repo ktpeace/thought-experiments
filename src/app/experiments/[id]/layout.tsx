c549b2d577361b5d3c157a010003e75900cf3938
Kat <katpeace@protonmail.com>
2024-07-27 14:14:22 -0400

    fix sliding images reloading entire page and jumping around

--- src/app/experiments/[id]/layout.tsx
+++ src/app/experiments/[id]/layout.tsx
@@ -1,30 +1,9 @@
-// components/Layout.js
-// import { useRouter } from "next/router";
-// import Link from "next/link";
-
 import SlidingImages from "@/components/SlidingImages";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  //   const router = useRouter();
-  //   const currentPath = router.pathname;
-
   return (
     <div className="mt-8 mb-12 px-4 sm:px-8 xl:px-24 flex flex-col justify-center items-center">
-      {/* <nav>
-        <ul className={styles.navList}>
-          {menuItems.map((item) => (
-            <li
-              key={item.path}
-              className={currentPath === item.path ? styles.active : ""}
-            >
-              <Link href={item.path}>{item.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav> */}
-
       {children}
-
       <SlidingImages />
     </div>
   );
