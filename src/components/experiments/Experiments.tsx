cc039f7db167ac02153635d3b0b95c325ae779b9
Kat <katpeace@protonmail.com>
2024-08-01 19:10:15 -0400

    try fix next api error

--- src/components/experiments/Experiments.tsx
+++ src/components/experiments/Experiments.tsx
@@ -55,33 +55,33 @@ const Experiments = () => {
 
   // Fetch & set experiments when query params change
   // This can happen via direct call of setQueryParams, or by navigation
-  useEffect(() => {
-    async function fetchExperiments() {
-      try {
-        // Reset experiments
-        setLoading(true);
-        setExperiments([]);
-        // setPageNumber(1);
-        // Fetch using params
-        const queryParams = buildParams();
-        const response = await fetch(`/api/experiments?${queryParams}`);
-        // Set experiments with response data
-        const data = await response.json();
-        setExperiments(data.experiments);
-        // setExperiments((prev) => [...prev, ...data.experiments]);
-      } catch (err) {
-        // Handle errors
-        console.error(err);
-        setLoading(false);
-        setError(
-          "An error occurred fetching the experiments. Please refresh, and feel free to contact me via About page if errors persist."
-        );
-      } finally {
-        // Stop loading spinner
-        setLoading(false);
-      }
+  async function fetchExperiments() {
+    try {
+      // Reset experiments
+      setLoading(true);
+      setExperiments([]);
+      // setPageNumber(1);
+      // Fetch using params
+      const queryParams = buildParams();
+      const response = await fetch(`/api/experiments?${queryParams}`);
+      // Set experiments with response data
+      const data = await response.json();
+      setExperiments(data.experiments);
+      // setExperiments((prev) => [...prev, ...data.experiments]);
+    } catch (err) {
+      // Handle errors
+      console.error(err);
+      setLoading(false);
+      setError(
+        "An error occurred fetching the experiments. Please refresh, and feel free to contact me via About page if errors persist."
+      );
+    } finally {
+      // Stop loading spinner
+      setLoading(false);
     }
+  }
 
+  useEffect(() => {
     fetchExperiments();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchString]);
